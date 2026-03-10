4928cfab703abe8c8ff13e5fb518d1d0fe428524
Sonit2277 <kumar.c.2468@gmail.com>
2026-03-10 22:41:43 +0530

    Add files via upload

---
+++ mad-libs generator(beginer).py
@@ -0,0 +1,9 @@
+name = input("select a name: ")
+place = input("select a place: ")
+adjective = input("select an adjective: ")
+animal = input("select a animal: ")
+verb= input("select a verb: ")
+object = input("select an object: ")
+food = input("select a food: ")
+story = f"One day {name} went to the {place} and saw a very {adjective} {animal} sitting near a {object}, and suddenly the {animal} started to {verb} in a funny way, which surprised everyone around, so {name} decided to sit down and share some {food} with the {animal}, and by the end of the day they both became good friends at the {place}."
+print(story)
